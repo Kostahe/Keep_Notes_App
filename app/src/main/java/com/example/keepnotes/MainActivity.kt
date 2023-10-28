46b3cff160f797065724246be3c16e7abc58549d
Konstantyn <gagarin2789@gmail.com>
2023-10-28 14:33:56 +0200

    Added navigation + Firebase

--- app/src/main/java/com/example/keepnotes/MainActivity.kt
+++ app/src/main/java/com/example/keepnotes/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.keepnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Konstantyn"
+        user["last"] = "Huzil"
+        user["born"] = 2006
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.e("TAG", "Error adding document", e)
+            }
     }
 }
