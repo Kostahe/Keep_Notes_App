d059ef529cedfa04aa183accd615093a113b5a42
Konstantyn <gagarin2789@gmail.com>
2023-11-01 22:09:08 +0100

    Created FireBaseModule and providedFireStore

--- app/src/main/java/com/example/keepnotes/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/example/keepnotes/data/repository/NoteRepositoryImpl.kt
@@ -2,9 +2,12 @@ package com.example.keepnotes.data.repository
 
 import com.example.keepnotes.data.model.Note
 import com.example.keepnotes.domain.repository.NoteRepository
+import com.google.firebase.firestore.FirebaseFirestore
 import javax.inject.Inject
 
-class NoteRepositoryImpl @Inject constructor(): NoteRepository {
+class NoteRepositoryImpl @Inject constructor(
+    private val database: FirebaseFirestore
+): NoteRepository {
     override fun getNotes(): List<Note> {
         return listOf()
     }
