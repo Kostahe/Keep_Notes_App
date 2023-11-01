ef0d1d1795a321d55d6812f24711e5731f38fe01
Konstantyn <gagarin2789@gmail.com>
2023-11-01 22:19:03 +0100

    Sealed class state done

--- app/src/main/java/com/example/keepnotes/domain/repository/State.kt
+++ app/src/main/java/com/example/keepnotes/domain/repository/State.kt
@@ -1,4 +1,7 @@
 package com.example.keepnotes.domain.repository
 
-sealed class State {
+sealed class State<T>(val data: T? = null, val message: String? = null) {
+    object Loading: State<Nothing>()
+    class Success<T>(data: T?): State<T>(data)
+    class Error<T>(message: String): State<T>(message = message)
 }
