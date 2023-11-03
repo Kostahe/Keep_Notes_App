9e0fc3d423b9c4556b490cd2d5f0737fcf8a0ddf
Konstantyn <gagarin2789@gmail.com>
2023-11-04 00:15:43 +0100

    Implemented NoteRepository functions

--- app/src/main/java/com/example/keepnotes/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/example/keepnotes/data/repository/NoteRepositoryImpl.kt
@@ -2,40 +2,50 @@ package com.example.keepnotes.data.repository
 
 import android.provider.ContactsContract.Data
 import com.example.keepnotes.data.model.Note
+import com.example.keepnotes.domain.repository.FireStoreTables
 import com.example.keepnotes.domain.repository.NoteRepository
 import com.example.keepnotes.domain.repository.State
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
 import java.util.Date
 import javax.inject.Inject
 
 class NoteRepositoryImpl @Inject constructor(
     private val database: FirebaseFirestore
 ): NoteRepository {
-    override fun getNotes(): State<List<Note>> {
-        val data = listOf<Note>(
-            Note(
-                id = "lorem1",
-                title = "Title1",
-                text = "Note 1",
-                date = Date()
-            ),
-            Note(
-                id = "lorem2",
-                title = "Title2",
-                text = "Note 2",
-                date = Date()
-            ),
-            Note(
-                id = "lorem3",
-                title = "Title3",
-                text = "Note 3",
-                date = Date()
-            )
-        )
-        return if (data.isNullOrEmpty()) {
-            State.Error("Data is empty")
-        } else {
-            State.Success(data)
-        }
+    override fun getNotes(result: (State<List<Note>>) -> Unit) {
+        database.collection(FireStoreTables.NOTE)
+            .get()
+            .addOnSuccessListener {
+                val notes: MutableList<Note> = mutableListOf()
+                it.forEach { document ->
+                    notes.add(document.toObject(Note::class.java))
+                }
+                result.invoke(
+                    State.Success(notes)
+                )
+            }
+            .addOnFailureListener {
+                result.invoke(
+                    State.Error(it.localizedMessage.orEmpty())
+                )
+            }
     }
+
+
+    override fun addNotes(note: Note, result: (State<String>) -> Unit) {
+        database.collection(FireStoreTables.NOTE)
+            .add(note)
+            .addOnSuccessListener {
+                result.invoke(
+                    State.Success(it.id)
+                )
+            }
+            .addOnFailureListener {
+                result.invoke(
+                    State.Error(it.localizedMessage.orEmpty())
+                )
+            }
+    }
+
 }
