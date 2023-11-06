94136ac12df9f89cf5ae8b2549873980ada46746
Konstantyn <gagarin2789@gmail.com>
2023-11-06 20:30:02 +0100

    Cleaned up code

--- app/src/main/java/com/example/keepnotes/di/AppComponent.kt
+++ app/src/main/java/com/example/keepnotes/di/AppComponent.kt
@@ -1,17 +1,15 @@
 package com.example.keepnotes.di
 
 import android.app.Application
-import com.example.keepnotes.MainActivity
 import com.example.keepnotes.ui.NoteDetailFragment
 import com.example.keepnotes.ui.NoteListingFragment
-import com.google.firebase.ktx.Firebase
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 
 @Singleton
-@Component(modules = [AppModule::class, ViewModelModule::class, RepositoryModule::class, FireBaseModule::class])
+@Component(modules = [ViewModelModule::class, RepositoryModule::class, FireBaseModule::class])
 interface AppComponent {
 
     fun injectToNoteListFragment(noteListingFragment: NoteListingFragment)
