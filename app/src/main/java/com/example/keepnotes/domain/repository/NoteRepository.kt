1db34b52998f4bdc374f2230f9d1c7fc5f35d18a
Konstantyn <gagarin2789@gmail.com>
2023-11-09 15:21:09 +0100

    Added function updateNote to NoteRepository

--- app/src/main/java/com/example/keepnotes/domain/repository/NoteRepository.kt
+++ app/src/main/java/com/example/keepnotes/domain/repository/NoteRepository.kt
@@ -5,4 +5,5 @@ import com.example.keepnotes.data.model.Note
 interface NoteRepository {
     fun getNotes( result: (State<List<Note>>) -> Unit)
     fun addNotes(note: Note, result: (State<String>) -> Unit)
+    fun updateNote(note: Note, result: (State<String>) -> Unit)
 }
