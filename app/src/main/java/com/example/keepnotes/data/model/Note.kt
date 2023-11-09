bf181cada08bfcd2176f331d9396d07670a82897
Konstantyn <gagarin2789@gmail.com>
2023-11-09 14:38:33 +0100

    Added Parcelize to class Note

--- app/src/main/java/com/example/keepnotes/data/model/Note.kt
+++ app/src/main/java/com/example/keepnotes/data/model/Note.kt
@@ -1,12 +1,15 @@
 package com.example.keepnotes.data.model
 
+import android.os.Parcelable
 import com.google.firebase.firestore.ServerTimestamp
+import kotlinx.parcelize.Parcelize
 import java.util.Date
 
+@Parcelize
 data class Note(
     var id: String = "",
     val title: String = "",
     val text: String = "",
     @ServerTimestamp
     val date: Date = Date()
-)
+): Parcelable
