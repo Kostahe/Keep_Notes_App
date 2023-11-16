fe9eb79d235fd11d1131c2dd9c164d533ef72503
Konstantyn <gagarin2789@gmail.com>
2023-11-17 00:02:34 +0100

    Updated WelcomeFragment and fragment_welcome.xml layout

--- app/src/main/java/com/example/keepnotes/ui/authentication/WelcomeFragment.kt
+++ app/src/main/java/com/example/keepnotes/ui/authentication/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.keepnotes.R
 import com.example.keepnotes.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -22,6 +24,17 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            registerButton.setOnClickListener {
+                findNavController().navigate(R.id.action_welcomeFragment_to_registrationFragment)
+            }
+
+            loginButton.setOnClickListener {
+                findNavController().navigate(R.id.action_welcomeFragment_to_loginFragment)
+            }
+
+        }
     }
 
 }
