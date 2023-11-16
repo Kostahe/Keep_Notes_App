412b20410dc8bd6a195846cf27d5c27628d3c2de
Konstantyn <gagarin2789@gmail.com>
2023-11-16 20:15:40 +0100

    Provided FirebaseAuthentication

--- app/src/main/java/com/example/keepnotes/data/repository/AuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/example/keepnotes/data/repository/AuthenticationRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.example.keepnotes.data.repository
 
 import com.example.keepnotes.domain.repository.AuthenticationRepository
+import com.google.firebase.auth.FirebaseAuth
 import javax.inject.Inject
 
-class AuthenticationRepositoryImpl @Inject constructor() : AuthenticationRepository {
+class AuthenticationRepositoryImpl @Inject constructor(
+    private val authentication: FirebaseAuth
+) : AuthenticationRepository {
 
 }
--- app/src/main/java/com/example/keepnotes/di/FireBaseModule.kt
+++ app/src/main/java/com/example/keepnotes/di/FireBaseModule.kt
@@ -1,5 +1,6 @@
 package com.example.keepnotes.di
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
@@ -12,5 +13,11 @@ object FireBaseModule {
     @Singleton
     fun provideFireStore(): FirebaseFirestore {
         return FirebaseFirestore.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthentication(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
     }
 }
