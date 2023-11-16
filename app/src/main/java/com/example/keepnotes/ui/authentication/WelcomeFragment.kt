48b5c78eb953c127fd74fdb5052731a6b9fbc339
Konstantyn <gagarin2789@gmail.com>
2023-11-16 22:05:01 +0100

    Added WelcomeFragment

---
+++ app/src/main/java/com/example/keepnotes/ui/authentication/WelcomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.keepnotes.ui.authentication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.keepnotes.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentWelcomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
