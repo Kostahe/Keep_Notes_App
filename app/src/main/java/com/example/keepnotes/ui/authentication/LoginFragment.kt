6ca616a3f079a3c4230c5f7642c562c7d3839552
Konstantyn <gagarin2789@gmail.com>
2023-11-17 00:03:16 +0100

    Created LoginFragment and fragment_login.xml layout

---
+++ app/src/main/java/com/example/keepnotes/ui/authentication/LoginFragment.kt
@@ -0,0 +1,37 @@
+package com.example.keepnotes.ui.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.keepnotes.R
+import com.example.keepnotes.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+
+        }
+    }
+}
