65896541f87dea57fa49e2a264c8bf4cfbf96a56
Konstantyn <gagarin2789@gmail.com>
2023-11-17 23:36:05 +0100

    Created function forgotPassword

--- app/src/main/java/com/example/keepnotes/domain/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/example/keepnotes/domain/repository/AuthenticationRepository.kt
@@ -6,4 +6,5 @@ interface AuthenticationRepository {
     fun register(email: String, password: String, user: User, result: (State<String>) -> Unit)
     fun updateUser(user: User, result: (State<String>) -> Unit)
     fun login(email: String, password: String, result: (State<String>) -> Unit)
+    fun forgotPassword(email: String, result: (State<String>) -> Unit)
 }
