ffd64b1d43f79eea79db12795a29b6f690481ab4
Konstantyn <gagarin2789@gmail.com>
2023-11-17 16:38:48 +0100

    Created functions for register, updateUser, login

--- app/src/main/java/com/example/keepnotes/domain/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/example/keepnotes/domain/repository/AuthenticationRepository.kt
@@ -1,4 +1,9 @@
 package com.example.keepnotes.domain.repository
 
+import com.example.keepnotes.data.model.User
+
 interface AuthenticationRepository {
+    fun register(email: String, password: String, user: User, result: (State<String>) -> Unit)
+    fun updateUser(user: User, result: (State<String>) -> Unit)
+    fun login(user: User, password: String, result: (State<User>) -> Unit)
 }
