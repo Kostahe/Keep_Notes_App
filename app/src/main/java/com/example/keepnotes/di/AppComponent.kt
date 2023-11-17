3dae1a5a9d444f13fbb3d024a8898a5fc78b743b
Konstantyn <gagarin2789@gmail.com>
2023-11-17 22:05:18 +0100

    Injected AuthenticationViewModel to LoginFragment

--- app/src/main/java/com/example/keepnotes/di/AppComponent.kt
+++ app/src/main/java/com/example/keepnotes/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.keepnotes.di
 
 import android.app.Application
+import com.example.keepnotes.ui.authentication.LoginFragment
 import com.example.keepnotes.ui.authentication.RegistrationFragment
 import com.example.keepnotes.ui.note.NoteDetailFragment
 import com.example.keepnotes.ui.note.NoteListingFragment
@@ -16,6 +17,7 @@ interface AppComponent {
     fun injectToNoteListFragment(noteListingFragment: NoteListingFragment)
     fun injectToNoteDetailFragment(noteDetailFragment: NoteDetailFragment)
     fun injectToRegistrationFragment(registrationFragment: RegistrationFragment)
+    fun injectToLoginFragment(loginFragment: LoginFragment)
 
     @Component.Builder
     interface Builder {
--- app/src/main/java/com/example/keepnotes/ui/authentication/LoginFragment.kt
+++ app/src/main/java/com/example/keepnotes/ui/authentication/LoginFragment.kt
@@ -5,22 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.keepnotes.appComponent
 import com.example.keepnotes.databinding.FragmentLoginBinding
+import com.example.keepnotes.di.ViewModelFactory
+import javax.inject.Inject
 
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var viewModel: AuthenticationViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        context?.appComponent?.injectToLoginFragment(this)
+        viewModel = ViewModelProvider(this, viewModelFactory)[AuthenticationViewModel::class.java]
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(layoutInflater)
         return binding.root
     }
