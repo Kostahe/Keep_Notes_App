fb21e11f5812aae9e051475a918d66efa0dba28e
Konstantyn <gagarin2789@gmail.com>
2023-11-17 21:56:41 +0100

    Changed parameter result in login function

--- app/src/main/java/com/example/keepnotes/domain/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/example/keepnotes/domain/repository/AuthenticationRepository.kt
@@ -5,5 +5,5 @@ import com.example.keepnotes.data.model.User
 interface AuthenticationRepository {
     fun register(email: String, password: String, user: User, result: (State<String>) -> Unit)
     fun updateUser(user: User, result: (State<String>) -> Unit)
-    fun login(user: User, password: String, result: (State<User>) -> Unit)
+    fun login(email: String, password: String, result: (State<String>) -> Unit)
 }
