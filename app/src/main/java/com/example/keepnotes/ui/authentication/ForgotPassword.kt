a6fa64083469da8e042051e13c247f594fedfc31
Konstantyn <gagarin2789@gmail.com>
2023-11-17 23:40:49 +0100

    Created ForgotPassword fragment

---
+++ app/src/main/java/com/example/keepnotes/ui/authentication/ForgotPassword.kt
@@ -0,0 +1,28 @@
+package com.example.keepnotes.ui.authentication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.keepnotes.R
+
+
+class ForgotPassword : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
