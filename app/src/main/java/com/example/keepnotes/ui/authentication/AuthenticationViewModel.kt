b8d39ac31259ed51f3abb55aa473257ed870c29c
Konstantyn <gagarin2789@gmail.com>
2023-11-17 17:28:33 +0100

    Added register function to AuthenticationViewModel

--- app/src/main/java/com/example/keepnotes/ui/authentication/AuthenticationViewModel.kt
+++ app/src/main/java/com/example/keepnotes/ui/authentication/AuthenticationViewModel.kt
@@ -1,11 +1,28 @@
 package com.example.keepnotes.ui.authentication
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.keepnotes.data.model.User
 import com.example.keepnotes.domain.repository.AuthenticationRepository
+import com.example.keepnotes.domain.repository.State
 import javax.inject.Inject
 
 class AuthenticationViewModel @Inject constructor(
     private val repository: AuthenticationRepository
-): ViewModel() {
+) : ViewModel() {
+    private val _register = MutableLiveData<State<String>>()
+    val register: LiveData<State<String>>
+        get() = _register
 
+    fun register(
+        email: String,
+        password: String,
+        user: User
+    ) {
+        _register.value = State.Loading()
+        repository.register(email, password, user) {
+            _register.value = it
+        }
+    }
 }
