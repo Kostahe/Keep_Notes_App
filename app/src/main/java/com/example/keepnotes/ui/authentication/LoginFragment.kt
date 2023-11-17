99c0be4d6f5210f01556d83825b899e37fae5a1c
Konstantyn <gagarin2789@gmail.com>
2023-11-17 22:47:26 +0100

    Added validation function to LoginFragment and observed login

--- app/src/main/java/com/example/keepnotes/ui/authentication/LoginFragment.kt
+++ app/src/main/java/com/example/keepnotes/ui/authentication/LoginFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.keepnotes.R
 import com.example.keepnotes.appComponent
 import com.example.keepnotes.databinding.FragmentLoginBinding
 import com.example.keepnotes.di.ViewModelFactory
+import com.example.keepnotes.domain.repository.State
 import javax.inject.Inject
 
 
@@ -37,7 +40,57 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            loginButton.setOnClickListener {
+                if (validation()) {
+                    viewModel.login(
+                        email = emailEditText.text.toString(),
+                        password = passwordEditText.text.toString()
+                    )
+                }
 
+                viewModel.login.observe(viewLifecycleOwner) { state ->
+                    when (state) {
+                        is State.Loading -> {
+                            progressBar.visibility = View.VISIBLE
+                        }
+
+                        is State.Error -> {
+                            progressBar.visibility = View.INVISIBLE
+                        }
+
+                        is State.Success -> {
+                            progressBar.visibility = View.INVISIBLE
+                            findNavController().navigate(R.id.action_loginFragment_to_noteListingFragment)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun validation(): Boolean {
+        var isValid = true
+
+        binding.apply {
+            if (emailEditText.text.toString().trim().isEmpty()) {
+                isValid = false
+            } else if (" " in emailEditText.text.toString()) {
+                isValid = false
+            } else if (android.util.Patterns.EMAIL_ADDRESS.matcher(emailEditText.text.toString())
+                    .matches()
+            ) {
+                isValid = false
+            }
+
+            if (passwordEditText.text.toString().trim().isEmpty()) {
+                isValid = false
+            } else if (" " in passwordEditText.text.toString()) {
+                isValid = false
+            } else if (passwordEditText.text.toString().length < 8) {
+                isValid = false
+            }
+
+            return isValid
         }
     }
 }
