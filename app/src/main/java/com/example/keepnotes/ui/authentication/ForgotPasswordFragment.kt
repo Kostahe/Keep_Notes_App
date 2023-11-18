3b83d4ee4671b3902890e708028739a5137c9c32
Konstantyn <gagarin2789@gmail.com>
2023-11-18 21:14:56 +0100

    Updated ForgotPasswordFragment

--- app/src/main/java/com/example/keepnotes/ui/authentication/ForgotPasswordFragment.kt
+++ app/src/main/java/com/example/keepnotes/ui/authentication/ForgotPasswordFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.keepnotes.R
 import com.example.keepnotes.appComponent
+import com.example.keepnotes.databinding.FragmentForgotPasswordBinding
 import com.example.keepnotes.di.ViewModelFactory
+import com.example.keepnotes.domain.repository.State
 import javax.inject.Inject
 
 
@@ -17,6 +21,8 @@ class ForgotPasswordFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
     private lateinit var viewModel: AuthenticationViewModel
+
+    private lateinit var binding: FragmentForgotPasswordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +33,64 @@ class ForgotPasswordFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    ): View {
+        binding = FragmentForgotPasswordBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+
+            emailEditText.addTextChangedListener {
+                emailEditTextLayout.error = null
+            }
+
+            sendButton.setOnClickListener {
+                if (validation()) {
+                    viewModel.forgotPassword(
+                        email = emailEditText.text.toString()
+                    )
+                }
+            }
+
+            viewModel.forgotPassword.observe(viewLifecycleOwner) { state ->
+                when (state) {
+                    is State.Loading -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+                    is State.Error -> {
+                        progressBar.visibility = View.INVISIBLE
+                        Toast.makeText(context, state.message, Toast.LENGTH_SHORT).show()
+                    }
+                    is State.Success -> {
+                        progressBar.visibility = View.INVISIBLE
+                    }
+                }
+            }
+        }
+    }
+
+    private fun validation(): Boolean {
+        var isValid = true
+        binding.apply {
+            if (emailEditText.text.toString().trim().isEmpty()) {
+                isValid = false
+                emailEditTextLayout.error = getString(R.string.email_can_not_be_empty)
+
+            } else if (" " in emailEditText.text.toString()) {
+                isValid = false
+                emailEditTextLayout.error = getString(R.string.email_can_not_contain_spaces)
+            } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(
+                    emailEditText.text.toString().trim()
+                ).matches()
+            ) {
+                isValid = false
+                emailEditTextLayout.error = getString(R.string.the_email_format_is_not_correct)
+            }
+        }
+
+        return isValid
     }
 }
