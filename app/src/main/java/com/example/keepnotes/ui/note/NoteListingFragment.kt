073efc3ec305a618b456a0112d12bc98deeba67e
Konstantyn <gagarin2789@gmail.com>
2023-11-19 17:58:39 +0100

    Implemented MenuItemClickListener

--- app/src/main/java/com/example/keepnotes/ui/note/NoteListingFragment.kt
+++ app/src/main/java/com/example/keepnotes/ui/note/NoteListingFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -15,6 +16,7 @@ import com.example.keepnotes.databinding.FragmentNoteListingBinding
 import com.example.keepnotes.di.ViewModelFactory
 import com.example.keepnotes.domain.repository.NavigationConstants
 import com.example.keepnotes.domain.repository.State
+import com.example.keepnotes.ui.authentication.AuthenticationViewModel
 import javax.inject.Inject
 
 
@@ -25,46 +27,59 @@ class NoteListingFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
     private lateinit var viewModel: NoteViewModel
+    private lateinit var authenticationViewModel: AuthenticationViewModel
 
     private val adapter by lazy {
-        NoteListingAdapter(
-            onItemCLicked = { _, item ->
-                findNavController().navigate(
-                    R.id.action_noteListingFragment_to_noteDetailFragment,
-                    Bundle().apply {
-                        putString(NavigationConstants.TYPE, NavigationConstants.UPDATE)
-                        putParcelable(NavigationConstants.NOTE, item)
-                    })
-            }
-        )
+        NoteListingAdapter(onItemCLicked = { _, item ->
+            findNavController().navigate(R.id.action_noteListingFragment_to_noteDetailFragment,
+                Bundle().apply {
+                    putString(NavigationConstants.TYPE, NavigationConstants.UPDATE)
+                    putParcelable(NavigationConstants.NOTE, item)
+                })
+        })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         context?.appComponent?.injectToNoteListFragment(this)
         viewModel = ViewModelProvider(this, viewModelFactory)[NoteViewModel::class.java]
+        authenticationViewModel =
+            ViewModelProvider(this, viewModelFactory)[AuthenticationViewModel::class.java]
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentNoteListingBinding.inflate(layoutInflater)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.searchBar)
+        binding.searchBar.inflateMenu(R.menu.menu)
+        binding.searchBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.log_out_button -> {
+                    authenticationViewModel.logout()
+                    findNavController().navigate(R.id.action_noteListingFragment_to_welcomeFragment)
+                    true
+                }
+
+                else -> false
+            }
+        }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.floatingActionButton.setOnClickListener {
-            findNavController().navigate(
-                R.id.action_noteListingFragment_to_noteDetailFragment,
+            findNavController().navigate(R.id.action_noteListingFragment_to_noteDetailFragment,
                 Bundle().apply {
                     putString(NavigationConstants.TYPE, NavigationConstants.CREATE)
                 })
         }
         binding.recyclerViewOfNotes.adapter = adapter
-        binding.recyclerViewOfNotes.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        binding.recyclerViewOfNotes.layoutManager =
+            StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
         viewModel.getNotes()
         viewModel.note.observe(viewLifecycleOwner) { noteState ->
             when (noteState) {
