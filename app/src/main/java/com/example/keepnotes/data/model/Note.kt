5515c4c06e0f3f14ff61bb3d730dd74b76a673cd
Konstantyn <gagarin2789@gmail.com>
2023-11-25 13:50:14 +0100

    Updated Note.kt

--- app/src/main/java/com/example/keepnotes/data/model/Note.kt
+++ app/src/main/java/com/example/keepnotes/data/model/Note.kt
@@ -8,8 +8,8 @@ import java.util.Date
 @Parcelize
 data class Note(
     var id: String = "",
+    var userId: String = "",
     val title: String = "",
     val text: String = "",
-    @ServerTimestamp
-    val date: Date = Date()
+    @ServerTimestamp val date: Date = Date()
 ) : Parcelable
