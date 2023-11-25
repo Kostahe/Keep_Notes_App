d29056cd14e98c3d927b6ab102136a3a8d6e3a51
Konstantyn <gagarin2789@gmail.com>
2023-11-25 13:50:58 +0100

    Updated NoteRepositoryImpl

--- app/src/main/java/com/example/keepnotes/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/example/keepnotes/data/repository/NoteRepositoryImpl.kt
@@ -1,19 +1,26 @@
 package com.example.keepnotes.data.repository
 
 
+import android.util.Log
 import com.example.keepnotes.data.model.Note
-import com.example.keepnotes.util.FireStoreTables
+import com.example.keepnotes.data.model.User
 import com.example.keepnotes.domain.repository.NoteRepository
 import com.example.keepnotes.domain.repository.State
+import com.example.keepnotes.util.FireStoreDocumentField
+import com.example.keepnotes.util.FireStoreTables
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import javax.inject.Inject
 
 class NoteRepositoryImpl @Inject constructor(
     private val database: FirebaseFirestore
 ) : NoteRepository {
-    override fun getNotes(result: (State<List<Note>>) -> Unit) {
-        val collectionRef = database.collection(FireStoreTables.NOTE)
 
+    override fun getNotes(user: User?, result: (State<List<Note>>) -> Unit) {
+        Log.d("NOTES LIST ID", user?.id.toString())
+        val collectionRef = database.collection(FireStoreTables.NOTE)
+            .whereEqualTo(FireStoreDocumentField.USER_ID, user?.id)
+            .orderBy(FireStoreDocumentField.DATE, Query.Direction.DESCENDING)
         collectionRef.addSnapshotListener { snapshot, error ->
             if (error != null) {
                 result.invoke(State.Error(error.message.orEmpty()))
@@ -24,13 +31,12 @@ class NoteRepositoryImpl @Inject constructor(
             snapshot?.forEach { document ->
                 notes.add(document.toObject(Note::class.java))
             }
-
+            Log.d("NOTES LIST", notes.toString())
             result.invoke(State.Success(notes))
         }
     }
 
-
-    override fun addNotes(note: Note, result: (State<String>) -> Unit) {
+    override fun addNote(note: Note, result: (State<String>) -> Unit) {
         val document = database.collection(FireStoreTables.NOTE).document()
 
         note.id = document.id
